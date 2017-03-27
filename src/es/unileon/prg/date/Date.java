428317a7cf37caf73659c54d060addf9c371aadb
jfernz00 <jfernz00@estudiantes.unileon.es>
2017-03-27 13:13:20 +0200

    Empiezo creando paquetes y archivos

---
+++ src/es/unileon/prg/date/Date.java
@@ -0,0 +1,47 @@
+package es.unileon.prg.date;
+
+public class Date {
+
+	private int day;
+	private int month;
+	private int year;
+	}
+
+	
+	public Date (int day, month, year){
+	
+		day=this.day;
+		month=this.month;
+		year=this.year;
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/es/unileon/prg/date/MainDate.java
@@ -0,0 +1,12 @@
+package es.unileon.prg.date;
+
+public static void main(String args[]){
+
+	
+	
+
+	Date today = new Date (27,3,2017);
+	Date tomorrow = new Date (28,3,2017);
+	
+	
+}
