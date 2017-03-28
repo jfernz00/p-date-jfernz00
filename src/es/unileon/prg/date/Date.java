e557eba6503ec93e39797ce98d2024094ce8c02b
jfernz00 <jfernz00@estudiantes.unileon.es>
2017-03-28 12:50:05 +0200

    seguimos con las nociones del profe de practicas

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -2,46 +2,58 @@ package es.unileon.prg.date;
 
 public class Date {
 
-	private int day;
-	private int month;
-	private int year;
+	int _day;
+	int _month;
+	int _year;
+	
+	
+	public Date(int day, int month, int year){
+		
+		_day = day;
+		_month = month;
+		_year = year;
 	}
 
-	
-	public Date (int day, month, year){
-	
-		day=this.day;
-		month=this.month;
-		year=this.year;
-	
-	
+	public int getDay(){
+		return _day;
+	}
 
+	public int getMonth(){
+		return _month;
+	}
 
+	public int getYear(){
+		return _year;
+	}
 
+//SAME_YEAR
+	public boolean isSameYear(int year){
+		boolean same=false;
+		if(_year==year){
+			same=true;
+		}
+		return same;
+	}
 
+//SAME_MONTH
+	public boolean isSameMonth(int month){
+		boolean same=false;
+		if(_month==month){
+			same =true;
+		}
+		return same;
 
+	}
 
+//SAME_DAY
+	public boolean isSameDay(int day){
+		boolean same=false;
+		if(_day==day){
+			same =true;
+		}
+		return same;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
 }
+
+
--- src/es/unileon/prg/date/MainDate.java
+++ src/es/unileon/prg/date/MainDate.java
@@ -1,12 +1,23 @@
 package es.unileon.prg.date;
 
-public static void main(String args[]){
+
+public class MainDate{
+	
+	public static void main(String args[]){
 
 	
+	Date today = new Date (28,3,2017);
+	Date other = new Date (28,3,1492);
+	
+	System.out.println("Tienen el mismo dia? -- "+today.isSameDay(other.getDay()));
+
+	System.out.println("Tienen el mismo MES? -- "+today.isSameMonth(other.getMonth()));
+	
+	System.out.println("Tienen el mismo AÑO? -- "+today.isSameYear(other.getYear()));
 	
 
-	Date today = new Date (27,3,2017);
-	Date tomorrow = new Date (28,3,2017);
-	
-	
+
+
+
+	}	
 }
