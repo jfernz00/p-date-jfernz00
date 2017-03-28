692e1e9c3b93bb56e4486b0d029b243a806062b1
jfernz00 <jfernz00@estudiantes.unileon.es>
2017-03-29 00:40:10 +0200

    hasta check month

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -64,8 +64,61 @@ int _year;
 		return same;
 	}
 
+	public String getMonthName(int month){
+		String cadMonth="";
+		
+		switch(month){
+			case 1: cadMonth="Enero";
+				break;
+			case 2: cadMonth="Febrero";
+				break;
+			case 3: cadMonth="Marzo";
+				break;
+			case 4: cadMonth="Abril";
+				break;
+			case 5: cadMonth="Mayo";
+				break;
+			case 6: cadMonth="Junio";
+				break;
+			case 7: cadMonth="Julio";
+				break;
+			case 8: cadMonth="Agosto";
+				break;
+			case 9: cadMonth="Septiembre";
+				break;
+			case 10: cadMonth="Octubre";
+				break;
+			case 11: cadMonth="Noviembre";
+				break;
+			case 12: cadMonth="Diciembre";
+				break;
+		}
+		return cadMonth;
+	}
+	
+	public String checkMonthDays(int month){
+		int numDay=0;
+		switch(month){
+			
+			case 2:  numDay=28;
+				break;
+			case 4: 
+			case 6: 
+			case 9:
+			case 11:  numDay=30;
+				break;
+			default: numDay=31;
+		}
+
+		String check="Checked... CORRECT";
+		if(numDay<=_day){
+			check="Checked... INCORRECT NUMBER OF DAYS";
+		}
+		return check;
+	
 
 
+	}
 
 
 
--- src/es/unileon/prg/date/MainDate.java
+++ src/es/unileon/prg/date/MainDate.java
@@ -6,7 +6,7 @@ public class MainDate{
 	public static void main(String args[]){
 
 	
-	Date today = new Date (28,3,2017);
+	Date today = new Date (29,3,2017);
 //	Date other = new Date (28,3,2017);
 	Date other = new Date (28,3,1942);
 
@@ -27,12 +27,14 @@ public class MainDate{
 	
 	System.out.println("Tienen la misma FECHA? - "+today.isSame(other.getDay(),other.getMonth(),other.getYear()));
 
+System.out.println(" ");
 	
 //Ejercicios DIAPOSITIVA-2
-	System.out.println(" ----- PARTE 2 ----- ")
+	System.out.println(" ----- PARTE 2 ----- ");
 
+	System.out.println("Nombre del mes introducido - "+today.getMonthName(today.getMonth()));
 
-
+	System.out.println("Checking Mes ... - "+today.checkMonthDays(today.getMonth()));
 
 	}	
 }
